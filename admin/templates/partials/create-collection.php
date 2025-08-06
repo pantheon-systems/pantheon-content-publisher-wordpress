139aeccceea4ded537c099dd15690ddb4b99e3f1
Anais Gueyte <anaisgueyte@pantheon.io>
2025-08-06 16:54:37 -0400

    Attempting to solve codacy issues

--- admin/templates/partials/create-collection.php
+++ admin/templates/partials/create-collection.php
@@ -23,7 +23,7 @@ if (!defined('ABSPATH')) {
 						A content collection is a set of content for your WordPress site.
 						Connected to Google Workspace,
 						it helps you organize and manage your site content in Google Docs.',
-						   'pantheon-content-publisher') ?>
+							'pantheon-content-publisher') ?>
 						</p>
 						<p class="text-with-border mb-[1.875rem]"><?php echo esc_url(site_url()) ?></p>
 						<p class="text-lg font-bold mb-[1.25rem]" >
@@ -67,7 +67,7 @@ if (!defined('ABSPATH')) {
 						<div class="mb-10">
 							<a class="secondary-button self-start justify-self-end" id="pcc-disconnect" href="#">
 								<?php esc_html_e(
-									'Reset your Google Workspace authentication', 'pantheon-content-publisher') ?>
+								'Reset your Google Workspace authentication', 'pantheon-content-publisher') ?>
 							</a>
 						</div>
 					</div>
