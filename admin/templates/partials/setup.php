a77e73edee5cda3c8d56040a1dcabf580e4abf8d
Andrew Glago <andrewglago1@gmail.com>
2025-08-07 12:12:40 +0000

    refactor: update code formatting and improve readability to resolve phpcs warnings

--- admin/templates/partials/setup.php
+++ admin/templates/partials/setup.php
@@ -17,7 +17,12 @@ if (!defined('ABSPATH')) {
 					<div class="col-span-8">
 						<div class="w-[80%]">
 							<h1 class="page-header">
-								<?php esc_html_e('Connect Google Workspace to your WordPress site', 'pantheon-content-publisher-for-wordpress') ?>
+								<?php
+								esc_html_e(
+									'Connect Google Workspace to your WordPress site',
+									'pantheon-content-publisher-for-wordpress'
+								)
+								?>
 							</h1>
 							<p class="page-description">
 								<?php
@@ -28,29 +33,32 @@ if (!defined('ABSPATH')) {
 								) ?>
 							</p>
 							<div class="mt-8 mb-0.5">
-							<span class="font-semibold text-[0.83rem]">
-								<?php esc_html_e('Management token', 'pantheon-content-publisher-for-wordpress') ?>
-							</span>
+								<span class="font-semibold text-[0.83rem]">
+									<?php esc_html_e('Management token', 'pantheon-content-publisher-for-wordpress') ?>
+								</span>
 								<img class="scale-110 ms-1 pb-2.5 inline"
-									 src="<?php echo esc_url(PCC_PLUGIN_DIR_URL . 'assets/images/red-dot.svg') ?>"
-									 alt="Red Dot Icon">
+									src="<?php echo esc_url(PCC_PLUGIN_DIR_URL . 'assets/images/red-dot.svg') ?>"
+									alt="Red Dot Icon">
 								<div class="tooltip inline">
 									<img class="scale-110 ms-2 pb-1 inline"
-										 src="<?php echo
-											esc_url(PCC_PLUGIN_DIR_URL . 'assets/images/circle-info.svg') ?>"
-										 alt="Circle Info">
+										src="<?php echo
+													esc_url(PCC_PLUGIN_DIR_URL . 'assets/images/circle-info.svg') ?>"
+										alt="Circle Info">
 									<span class="tooltip-text">
-									<?php
-									esc_html_e('Enter the management token obtained from
-                                    the Pantheon Content Publisher dashboard', 'pantheon-content-publisher-for-wordpress') ?>
-								</span>
+										<?php
+										esc_html_e(
+											'Enter the management token obtained from
+                                    the Pantheon Content Publisher dashboard',
+											'pantheon-content-publisher-for-wordpress'
+										) ?>
+									</span>
 								</div>
 
 							</div>
 							<input type="password"
-								   placeholder="***************"
-								   id="access-token"
-								   name="access_token" class="input-with-border mb-2" required/>
+								placeholder="***************"
+								id="access-token"
+								name="access_token" class="input-with-border mb-2" required />
 							<button id="pcc-app-authenticate" class="primary-button">
 								<?php esc_html_e('Connect', 'pantheon-content-publisher-for-wordpress') ?>
 							</button>
@@ -63,11 +71,11 @@ if (!defined('ABSPATH')) {
 									// making it link to the Pantheon Content Publisher dashboard.
 									__(
 										"Don't have a token yet? Go to the " .
-										"<a %s>Pantheon Content Publisher dashboard</a> to generate one.",
+											"<a %s>Pantheon Content Publisher dashboard</a> to generate one.",
 										'pantheon-content-publisher-for-wordpress'
 									),
 									'class="pantheon-link  hover:text-secondary" target="_blank" ' .
-									'href="https://content.pantheon.io/dashboard/settings/tokens?tab=1"'
+										'href="https://content.pantheon.io/dashboard/settings/tokens?tab=1"'
 								)
 							)
 							?>
@@ -75,7 +83,7 @@ if (!defined('ABSPATH')) {
 					</div>
 					<div class="col-span-4 self-start justify-self-end">
 						<img src="<?php echo esc_url(PCC_PLUGIN_DIR_URL . 'assets/images/multi-icons.png') ?>"
-							 alt="Pantheon Logo">
+							alt="Pantheon Logo">
 					</div>
 				</div>
 				<?php
