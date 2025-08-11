a5829f278204aa6b47c37e5dc674058701e5b850
Andrew Glago <andrewglago1@gmail.com>
2025-08-11 18:44:09 +0000

    refactor: fix lint errors

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -8,31 +8,31 @@
 
 $_tests_dir = getenv('WP_TESTS_DIR');
 if (! $_tests_dir) {
-  $_tests_dir = '/tmp/wordpress-tests-lib';
+	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
 if (!file_exists($_tests_dir . '/includes/functions.php')) {
-  fwrite(STDERR, "Could not find WordPress tests bootstrap in " . $_tests_dir . "\n");
-  exit(1);
+	fwrite(STDERR, "Could not find WordPress tests bootstrap in " . $_tests_dir . "\n");
+	exit(1);
 }
 
 // Preload Composer autoloader and Yoast PHPUnit Polyfills if available
 $projectRoot = dirname(__DIR__, 2);
 $vendorAutoload = $projectRoot . '/vendor/autoload.php';
 if (file_exists($vendorAutoload)) {
-  require $vendorAutoload;
+	require $vendorAutoload;
 }
 $yoastPolyfills = $projectRoot . '/vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
 if (file_exists($yoastPolyfills) && !defined('WP_TESTS_PHPUNIT_POLYFILLS_PATH')) {
-  define('WP_TESTS_PHPUNIT_POLYFILLS_PATH', $yoastPolyfills);
+	define('WP_TESTS_PHPUNIT_POLYFILLS_PATH', $yoastPolyfills);
 }
 
 require $_tests_dir . '/includes/functions.php';
 
 if (function_exists('tests_add_filter')) {
-  tests_add_filter('muplugins_loaded', function () use ($projectRoot) {
-    require $projectRoot . '/pantheon-content-publisher-for-wordpress.php';
-  });
+	tests_add_filter('muplugins_loaded', function () use ($projectRoot) {
+		require $projectRoot . '/pantheon-content-publisher-for-wordpress.php';
+	});
 }
 
 // Start up the WP testing environment.
