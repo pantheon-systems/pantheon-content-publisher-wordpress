80158cae6a0f9886fbf9bdf19cfec7ad3fe19c5a
Anais Gueyte <anaisgueyte@pantheon.io>
2025-08-18 08:53:20 -0400

    [SITE-5158] Update name and zip file name (#126)
    * Update repo name
    
    * Update release zip name

--- .github/release.sh
+++ .github/release.sh
@@ -16,7 +16,9 @@ INCLUDES=(
 
 # Determine repository name from the current directory.
 REPO=$(basename "$(pwd)")
-ZIP="${REPO}.zip"
+# Zip file to match plugin name rather then repo name
+PLUGIN_NAME="pantheon-content-publisher"
+ZIP="${PLUGIN_NAME}.zip"
 
 echo "Starting build process for $REPO..."
 
