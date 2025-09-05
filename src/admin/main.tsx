913d3f18344d0194e7af852f0e0f4e4e3e6c57a3
Andrew Glago <andrewglago1@gmail.com>
2025-09-05 18:39:22 +0000

    feat: add scoping of styles to prevent conflicts with admin styles

--- src/admin/main.tsx
+++ src/admin/main.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/index.css";
 import { HashRouter } from "react-router-dom";
+
+import "./styles/index.css";
+import "@pantheon-systems/pds-toolkit-react/css/pds-core.css";
+import "@pantheon-systems/pds-toolkit-react/css/pds-components.css";
 
 const container = document.getElementById("content-pub-root");
 if (container) {
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
+import postcssPrefixSelector from "postcss-prefix-selector";
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
@@ -28,7 +29,35 @@ export default defineConfig(({ mode }) => ({
   },
   css: {
     postcss: {
-      plugins: [tailwindcss(), autoprefixer()],
+      plugins: [
+        tailwindcss(),
+        autoprefixer(),
+        postcssPrefixSelector({
+          prefix: "#content-pub-root",
+          transform: (prefix: string, selector: string) => {
+            if (selector.startsWith("#content-pub-root")) return selector;
+
+            if (
+              selector === "html" ||
+              selector === "body" ||
+              selector === ":root"
+            ) {
+              return "#content-pub-root";
+            }
+
+            let cleaned = selector
+              .replace(/^html\s+/, "")
+              .replace(/^body\s+/, "")
+              .replace(/^:root\s+/, "");
+
+            if (cleaned.length === 0) {
+              return "#content-pub-root";
+            }
+
+            return `${prefix} ${cleaned}`;
+          },
+        }),
+      ],
     },
   },
 }));
